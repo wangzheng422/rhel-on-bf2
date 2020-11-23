0023f572b9aaa61fd1314f3eab3edf11298ba736
Fabrizio D'Angelo <fdangelo@redhat.com>
2020-11-23 11:54:26 -0500

    Rshim script updates
    Documentation + dependency and typo fix
    
    Signed-off-by: Fabrizio D'Angelo <fdangelo@redhat.com>

--- provision/bf2/rshim_userspace_install.sh
+++ provision/bf2/rshim_userspace_install.sh
@@ -6,8 +6,17 @@
 # Local host:
 # 	./rshim_userspace_install.sh
 #
+# The card should be accessible by /dev/ttyUSB0 like this:
+# minicom --color on --baudrate 115200 --device /dev/ttyUSB0
+#
+# Once the rshim drivers have been succesfully installed, /dev/rshim0 should have been created.
+# To prepare the card for provisioning, use the following commands:
+# echo BOOT_MODE 1 > /dev/rshim0/misc
+# echo SW_RESET 1 > /dev/rshim0/misc
+#
+#
 
-yum install -y automake autoconf elfutils-libelf-devel fuse-devel git kernel-modules-extra libusb-devel make pciutils-devel rpm-build tmux
+yum install -y automake autoconf elfutils-libelf-devel fuse-devel gcc git kernel-modules-extra libusb-devel make pciutils-devel rpm-build tmux
 cd /tmp || exit 1
 git clone https://github.com/Mellanox/rshim-user-space.git
 cd rshim-user-space || exit 1
@@ -22,5 +31,5 @@ git archive --format=tgz --prefix=rshim-"${version}"/ HEAD > $rpm_topdir/SOURCES
 rpmbuild -ba --nodeps --define "_topdir $rpm_topdir" --define 'dist %{nil}' *.spec
 
 rpm -ivh $rpm_topdir/RPMS/*/*rpm
-stystemctl enable --now rshim
+systemctl enable --now rshim
 systemctl status rshim
