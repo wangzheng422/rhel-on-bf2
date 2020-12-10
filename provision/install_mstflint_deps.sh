a26ffdfd4d63041520e56ef9b429fcca8d60323e
Balazs Nemeth <bnemeth@redhat.com>
2020-12-10 15:27:29 +0100

    Make install_mstflint_deps arch agnostic

--- provision/install_mstflint_deps.sh
+++ provision/install_mstflint_deps.sh
@@ -1,13 +1,27 @@
 #!/bin/env bash
 
+set -o errexit
+
 # Assuming this script is run from a container based on
 # registry.redhat.io/rhel8/support-tools like the toolbox container in openshift
+
+# podman login registry.redhat.io # and use your access.redhat.com credentials
+# uuid=$(podman pull registry.redhat.io/rhel8/support-tools)
+# podman run -it $uuid
+# curl -kO https://gitlab.cee.redhat.com/egarver/smart-nic-poc/-/raw/master/provision/install_mstflint_in_toolbox.sh
+# chmod +x install_mstflint_in_toolbox.sh
+
+if [ "$(uname -m)" = aarch64 ]; then
+    MFT_VER_PART=arm64
+else
+    MFT_VER_PART=x86_64
+fi
 
 curl -kO https://download-node-02.eng.bos.redhat.com/brewroot/packages/kernel/4.18.0/240.el8/$(uname -m)/kernel-devel-4.18.0-240.el8.$(uname -m).rpm
 curl -kO https://download-node-02.eng.bos.redhat.com/brewroot/packages/zstd/1.4.4/1.el8/$(uname -m)/zstd-1.4.4-1.el8.$(uname -m).rpm
 curl -kO https://gitlab.cee.redhat.com/egarver/smart-nic-poc/-/raw/master/provision/bf2/mst.sh
-curl -kO https://download-node-02.eng.bos.redhat.com/brewroot/packages/kernel/4.18.0/240.el8/aarch64/kernel-modules-4.18.0-240.el8.aarch64.rpm
-curl -kO https://linux.mellanox.com/public/repo/mlnx_ofed/5.1-2.3.7.1/rhel8.1/aarch64/mft-4.15.1-9.arm64.rpm
+curl -kO https://download-node-02.eng.bos.redhat.com/brewroot/packages/kernel/4.18.0/240.el8/$(uname -m)/kernel-modules-4.18.0-240.el8.$(uname -m).rpm
+curl -kO https://linux.mellanox.com/public/repo/mlnx_ofed/5.1-2.3.7.1/rhel8.1/$(uname -m)/mft-4.15.1-9.$MFT_VER_PART.rpm
 
 curl -kO https://download-node-02.eng.bos.redhat.com/brewroot/packages/kernel/4.18.0/240.el8/$(uname -m)/kernel-4.18.0-240.el8.$(uname -m).rpm
 curl -kO https://download-node-02.eng.bos.redhat.com/brewroot/packages/kernel/4.18.0/240.el8/$(uname -m)/kernel-core-4.18.0-240.el8.$(uname -m).rpm
@@ -26,5 +40,3 @@ rpm-build-4.14.3-4.el8.$(uname -m).rpm \
 zstd-1.4.4-1.el8.$(uname -m).rpm \
 usbutils-010-3.el8.$(uname -m).rpm
 
-chmod +x mst.sh
-./mst.sh --install
