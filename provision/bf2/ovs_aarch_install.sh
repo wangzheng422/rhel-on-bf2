4817b10e6dcfb4562cc08aa39115708e939ed65a
Eric Garver <egarver@redhat.com>
2021-01-28 10:43:45 -0500

    improvement(bf2): simplify OVS install

--- provision/bf2/ovs_aarch_install.sh
+++ provision/bf2/ovs_aarch_install.sh
@@ -14,22 +14,18 @@ grep -q "sslverify=false" /etc/yum.conf || echo "sslverify=false" >> /etc/yum.co
 
 VER_PART=$(uname -r | cut -d"-" -f2 | sed s/\.$(uname -m)//g)
 
-wget -P /tmp http://download.eng.bos.redhat.com/brewroot/vol/rhel-8/packages/openvswitch2.13/2.13.0/67.el8fdp/aarch64/openvswitch2.13-2.13.0-67.el8fdp.aarch64.rpm &> /dev/null &
-wget -P /tmp http://download.eng.bos.redhat.com/brewroot/vol/rhel-8/packages/openvswitch2.13/2.13.0/67.el8fdp/aarch64/python3-openvswitch2.13-2.13.0-67.el8fdp.aarch64.rpm &> /dev/null &
-wget -P /tmp http://download.eng.bos.redhat.com/brewroot/vol/rhel-8/packages/openvswitch2.13/2.13.0/67.el8fdp/aarch64/openvswitch2.13-devel-2.13.0-67.el8fdp.aarch64.rpm &> /dev/null &
-wget -P /tmp http://download.eng.bos.redhat.com/brewroot/vol/rhel-8/packages/openvswitch-selinux-extra-policy/1.0/23.el8fdp/noarch/openvswitch-selinux-extra-policy-1.0-23.el8fdp.noarch.rpm &> /dev/null &
-wget -P /tmp http://download-node-02.eng.bos.redhat.com/brewroot/packages/kernel/4.18.0/$VER_PART/aarch64/kernel-modules-extra-$(uname -r).rpm
+BREW_ROOT="http://download.eng.bos.redhat.com/brewroot/vol/rhel-8/packages/"
+OVS_RPMS="openvswitch2.13/2.13.0/67.el8fdp/aarch64/openvswitch2.13-2.13.0-67.el8fdp.aarch64.rpm
+          openvswitch2.13/2.13.0/67.el8fdp/aarch64/python3-openvswitch2.13-2.13.0-67.el8fdp.aarch64.rpm
+          openvswitch2.13/2.13.0/67.el8fdp/aarch64/openvswitch2.13-devel-2.13.0-67.el8fdp.aarch64.rpm
+          openvswitch-selinux-extra-policy/1.0/23.el8fdp/noarch/openvswitch-selinux-extra-policy-1.0-23.el8fdp.noarch.rpm"
 
-wait
+wget -c -P "/tmp/" -B "${BREW_ROOT}" ${OVS_RPMS}
+for RPM in ${OVS_RPMS}; do
+	dnf install -y "/tmp/${RPM#*/}"
+done
 
-cd /tmp || exit 1
-
-dnf install -y \
-openvswitch2.13-2.13.0-67.el8fdp.aarch64.rpm \
-python3-openvswitch2.13-2.13.0-67.el8fdp.aarch64.rpm \
-openvswitch2.13-devel-2.13.0-67.el8fdp.aarch64.rpm \
-openvswitch-selinux-extra-policy-1.0-23.el8fdp.noarch.rpm \
-kernel-modules-extra-$(uname -r).rpm
+dnf install -y kernel-modules-extra
 
 systemctl enable openvswitch
 systemctl start openvswitch
