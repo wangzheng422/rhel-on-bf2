c151501967e63e18679a1da837a194516307e14e
Eric Garver <egarver@redhat.com>
2021-01-28 10:52:20 -0500

    improvement(bf2): provision: use mstflint from brew

--- provision/bf2/bluefield_provision.sh
+++ provision/bf2/bluefield_provision.sh
@@ -15,24 +15,8 @@ function die {
 
 function mst_install {
 	status "Installing MST tools"
-	if [ "$(uname -m)" = aarch64 ]; then
-		MFT_VER=mft-4.15.1-9-arm64
-	else
-		MFT_VER=mft-4.15.1-9-x86_64
-	fi
-
-	yum install -y tar wget
-	yum install -y kernel-devel-"$(uname -r)" &
-	wget -c -P /tmp https://www.mellanox.com/downloads/MFT/$MFT_VER-rpm.tgz &>/dev/null &
-
-	wait
-
-	pushd /tmp
-	tar xf $MFT_VER-rpm.tgz
-	cd $MFT_VER-rpm
-	./install.sh
+	dnf install -y http://download.eng.bos.redhat.com/brewroot/vol/rhel-8/packages/mstflint/4.15.0/1.el8/x86_64/mstflint-4.15.0-1.el8.x86_64.rpm
 	mst start
-	popd
 }
 
 function rshim_install {
