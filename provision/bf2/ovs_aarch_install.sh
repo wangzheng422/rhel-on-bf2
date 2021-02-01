62bb256fe1f196864e89f2e4f2a896c11fde67b7
Eric Garver <egarver@redhat.com>
2021-02-01 10:26:35 -0500

    fix(bf2): ovs: bump version to -86
    To get all the ipsec goodies.

--- provision/bf2/ovs_aarch_install.sh
+++ provision/bf2/ovs_aarch_install.sh
@@ -15,10 +15,10 @@ grep -q "sslverify=false" /etc/yum.conf || echo "sslverify=false" >> /etc/yum.co
 VER_PART=$(uname -r | cut -d"-" -f2 | sed s/\.$(uname -m)//g)
 
 BREW_ROOT="http://download.eng.bos.redhat.com/brewroot/vol/rhel-8/packages/"
-OVS_RPMS="openvswitch2.13/2.13.0/67.el8fdp/aarch64/openvswitch2.13-2.13.0-67.el8fdp.aarch64.rpm
-          openvswitch2.13/2.13.0/67.el8fdp/aarch64/python3-openvswitch2.13-2.13.0-67.el8fdp.aarch64.rpm
-          openvswitch2.13/2.13.0/67.el8fdp/aarch64/openvswitch2.13-devel-2.13.0-67.el8fdp.aarch64.rpm
-          openvswitch-selinux-extra-policy/1.0/23.el8fdp/noarch/openvswitch-selinux-extra-policy-1.0-23.el8fdp.noarch.rpm"
+OVS_RPMS="openvswitch2.13/2.13.0/86.el8fdp/aarch64//openvswitch2.13-2.13.0-86.el8fdp.aarch64.rpm
+          openvswitch2.13/2.13.0/86.el8fdp/aarch64//python3-openvswitch2.13-2.13.0-86.el8fdp.aarch64.rpm
+          openvswitch2.13/2.13.0/86.el8fdp/aarch64//openvswitch2.13-devel-2.13.0-86.el8fdp.aarch64.rpm
+          openvswitch-selinux-extra-policy/1.0/28.el8fdp/noarch/openvswitch-selinux-extra-policy-1.0-28.el8fdp.noarch.rpm"
 
 wget -c -P "/tmp/" -B "${BREW_ROOT}" ${OVS_RPMS}
 for RPM in ${OVS_RPMS}; do
