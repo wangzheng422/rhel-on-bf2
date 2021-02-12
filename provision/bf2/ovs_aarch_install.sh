0f98ea4a14345c2449223f8edfb09705fd6c23cc
Balazs Nemeth <bnemeth@redhat.com>
2021-02-12 13:18:40 +0100

    Add ovs-ipsec to installation on bf2

--- provision/bf2/ovs_aarch_install.sh
+++ provision/bf2/ovs_aarch_install.sh
@@ -10,12 +10,11 @@
 # Ignore SSL certificate errors
 grep -q "sslverify=false" /etc/yum.conf || echo "sslverify=false" >> /etc/yum.conf
 
-VER_PART=$(uname -r | cut -d"-" -f2 | sed s/\.$(uname -m)//g)
-
 BREW_ROOT="http://download.eng.bos.redhat.com/brewroot/vol/rhel-8/packages/"
-OVS_RPMS="openvswitch2.13/2.13.0/86.el8fdp/aarch64//openvswitch2.13-2.13.0-86.el8fdp.aarch64.rpm
-          openvswitch2.13/2.13.0/86.el8fdp/aarch64//python3-openvswitch2.13-2.13.0-86.el8fdp.aarch64.rpm
-          openvswitch2.13/2.13.0/86.el8fdp/aarch64//openvswitch2.13-devel-2.13.0-86.el8fdp.aarch64.rpm
+OVS_RPMS="openvswitch2.13/2.13.0/86.el8fdp/aarch64/openvswitch2.13-2.13.0-86.el8fdp.aarch64.rpm
+          openvswitch2.13/2.13.0/86.el8fdp/aarch64/python3-openvswitch2.13-2.13.0-86.el8fdp.aarch64.rpm
+          openvswitch2.13/2.13.0/86.el8fdp/aarch64/openvswitch2.13-devel-2.13.0-86.el8fdp.aarch64.rpm
+          openvswitch2.13/2.13.0/86.el8fdp/aarch64/openvswitch2.13-ipsec-2.13.0-86.el8fdp.aarch64.rpm
           openvswitch-selinux-extra-policy/1.0/28.el8fdp/noarch/openvswitch-selinux-extra-policy-1.0-28.el8fdp.noarch.rpm"
 
 dnf install -y kernel-modules-extra $(for I in ${OVS_RPMS}; do echo ${BREW_ROOT}${I}; done)
